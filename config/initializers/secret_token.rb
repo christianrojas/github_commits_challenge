52c973ff49c6b2a06a460073f96f01a3575ce191
Christian Rojas <christianrojasgar@gmail.com>
2013-06-22 16:41:55 -0500

    Confirmacion inicial del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TangosourceChallenge::Application.config.secret_key_base = '53bbae855ef1ee935fdcc2d40855bfa487fb56ef4b75a4afd27bb79b7e7819e110ebe9ac3a1f75af3739e469d6920879a4d3180ab2d7a565fcb354a785e19ea0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TangosourceChallenge::Application.config.session_store :cookie_store, key: '_tangosource_challenge_session'
